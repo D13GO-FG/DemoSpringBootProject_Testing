703eb897def296b8bcb124de098ce4626c8491c8
Diego Flores <ldiego.flores98@gmail.com>
2023-03-24 15:01:34 -0700

    Testing database with mock using H2 database.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+CREATE TABLE Country (id INT PRIMARY KEY, country_name VARCHAR(50) , capital VARCHAR(50));
+INSERT INTO Country VALUES (1, 'Germany', 'Berlin');
+INSERT INTO Country VALUES (2, 'USA', 'Washington');
